0948fc5a3185e5050cfdd7425fd832c7268a4db3
Max <7enidomaks@gmail.com>
2023-12-17 18:01:40 +0300

    2 задание

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
+from TOKEN import token
+
 city_data=input("Введите город:")
+token=token
 def get_weather(city,token):
-    r=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=f2c3446a7fedc04e3848b74b7df782bf&units=metric")
+    r=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}&units=metric")
     data=r.json()
     city=data["name"]
     temperature=data['main']['temp']
     humid=data['main']["humidity"]
     pressure=data['main']['pressure']
-    print(f"Город: {city}"+"\n"+f"Температура сейчас: {temperature}°C"+"\n"+f"Влажность сейчас: {humid}%"+"\n"+f"Давление сейчас: {pressure}")
-get_weather(city_data,None)
+    return (f"Город: {city}"+"\n"+f"Температура сейчас: {temperature}°C"+"\n"+f"Влажность сейчас: {humid}%"+"\n"+f"Давление сейчас: {pressure}")
+print(get_weather(city_data,token))
